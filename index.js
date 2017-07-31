e184df4e29adcd6292b450bcb6486b41b90a702e
joan <joan@debianCours2.lan>
2017-08-01 01:36:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+
+  res.writeHead(200);
+
+  res.end('Salut tout le monde !');
+
+});
+
+server.listen(8080, function(){
+  var dateStart= new Date();
+  console.log(dateStart+" -  listening on *:8080")
+});
