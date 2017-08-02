bf8f1738196b7077e95024ac3103be6acd727542
joan <joan@debianCours2.lan>
2017-08-03 01:28:40 +0200

    ajout du style

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://172.16.12.154:8080/');
+var socket = io.connect(window.location.origin);
 
 socket.on('connect', function(data) {
 	socket.emit('join', 'user connected');
@@ -10,4 +10,8 @@ socket.on('unixVersion', function(data) {
 
 socket.on('serverInfo', function(data) {
 	$("#serverInfo").html(data);
+});
+
+socket.on('serverProcessus', function(data) {
+	$("#serverProcessus").html(data);
 });
--- src/infosystem.js
+++ src/infosystem.js
@@ -18,8 +18,22 @@ InfoSystem.prototype.sendServerInfo = function(){
 	exec(['top', '-b', '-d1', '-n1'], function(err, out, code) {
 		if (err instanceof Error)
 			throw err;
-		client.emit("serverInfo", out.replace("\n", "<br/>"));
+		
+		var result = out.replace(new RegExp("\n", 'g'), "<br/>\n");
+		var resultServerInfo= result.substr(0, result.indexOf( "<br/>\n<br/>\n"));
+		var resultServerProcessus= result.substr(result.indexOf( "<br/>\n<br/>\n")+14, result.length);
+		resultServerProcessus= "<span class='col-xs-1'>"
+								+resultServerProcessus.replace(new RegExp(" ", 'g'), "</span><span class='col-xs-1'>")
+													  .replace(new RegExp("\n", 'g'), "</span>\n<span class='col-xs-1'>")
+								+"</span>";
+		resultServerProcessus= resultServerProcessus.replace(new RegExp("<span class='col-xs-1'></span>", 'g'), "");
+		client.emit("serverInfo", resultServerInfo);
+		client.emit("serverProcessus", resultServerProcessus);
 	});
+	var infoSys = this;
+	setTimeout(function(){
+		infoSys.sendServerInfo();
+	},1000);
 }
 
 module.exports =  InfoSystem;
